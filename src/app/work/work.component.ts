b862de23a25fcfe05b1e5c7d61fb2ad66d41e247
Maik Langer <maiklanger@live.de>
2023-02-25 01:07:12 +0100

    add template for show pictures on screen

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
-  styleUrls: ['./work.component.scss']
+  styleUrls: ['./work.component.scss'],
 })
-export class WorkComponent {
+export class WorkComponent implements OnInit {
+  workImages = [];
 
+  ngOnInit() {
+    this.loadImages();
+    console.log(this.workImages);
+  }
+
+  loadImages() {
+    this.workImages = [
+      'preview-homepage.jpg',
+      'preview-join.jpg',
+      'preview-polo-loco.jpg',
+      'preview-ring-of-fire.png',
+    ];
+  }
 }
