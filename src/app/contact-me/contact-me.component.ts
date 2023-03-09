7ea7b9db561d51a202b4b3174172977d00988a82
Maik Langer <maiklanger@live.de>
2023-03-09 07:12:09 +0100

    add form validation

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,7 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
@@ -9,38 +8,49 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./contact-me.component.scss'],
 })
 export class ContactMeComponent {
-  formData = {
-    name: '',
-    email: '',
-    message: '',
-  };
+  contactForm: FormGroup;
 
-  email = new FormControl('', [Validators.required, Validators.email]);
+  constructor(public http: HttpClient) {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', Validators.required),
+    });
+  }
 
   getErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.contactForm.get('email').hasError('required')) {
       return 'You must enter a value';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.contactForm.get('email').hasError('email')
+      ? 'Not a valid email'
+      : '';
   }
 
-  constructor(public http: HttpClient) {}
-
   onSubmit() {
-    const formData = new FormData();
-    formData.append('name', this.formData.name);
-    formData.append('email', this.formData.email);
-    formData.append('message', this.formData.message);
+    if (this.contactForm.valid) {
+      // Formular ist gültig, sende E-Mail
+      const formData = new FormData();
+      formData.append('name', this.contactForm.get('name').value);
+      formData.append('email', this.contactForm.get('email').value);
+      formData.append('message', this.contactForm.get('message').value);
 
-    this.http
-      .post(
-        'https://maik-langer.developerakademie.net/send_mail/send_mail.php',
-        // 'https://web291.dogado.net/send_mail/send_mail.php',
-        formData
-      )
-      .subscribe((response) => {
-        console.log(response);
-      });
+      this.http
+        .post(
+          'https://maik-langer.developerakademie.net/send_mail/send_mail.php',
+          // 'https://web291.dogado.net/send_mail/send_mail.php',
+          formData
+        )
+        .subscribe((response) => {
+          console.log(response);
+          this.contactForm.reset();
+        });
+      alert('Your email has been sent. ');
+      window.location.reload();
+    } else {
+      // Formular ist ungültig, zeige Fehlermeldung an
+      alert('Please fill out all required fields.');
+    }
   }
 }
